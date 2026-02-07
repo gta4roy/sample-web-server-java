e4c457f6a0e2ee90568198e3c31af8a2d5763a5f
Abhijit <gta4roy@gmail.com>
2026-02-07 12:06:35 +0530

    update gradle

--- compile.sh
+++ compile.sh
@@ -2,4 +2,7 @@
 JAVA_HOME=/home/abhijit/tools/jdk-25.0.2
 GRADLE_HOME=/home/abhijit/tools/gradle-9.3.1
 
-$GRADLE_HOME/bin/gradle clean build 
+export JAVA_HOME=${JAVA_HOME}
+export GRADLE_HOME=${GRADLE_HOME}
+
+$GRADLE_HOME/bin/gradle clean bootJar
