9d4cd7d5ae36637df2870e30ccb2ec792bb70b6b
Abhijit <gta4roy@gmail.com>
2026-02-07 11:57:59 +0530

    updated code

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+JAVA_HOME=/home/abhijit/tools/jdk-25.0.2
+GRADLE_HOME=/home/abhijit/tools/gradle-9.3.1
+
+$GRADLE_HOME/bin/gradle clean build 
---
+++ src/main/java/com/gta4roy/java/JavaWebServer/MainController.java
@@ -0,0 +1,13 @@
+package com.gta4roy.java.JavaWebServer;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    public static String getHealth(){
+        JSONObject object = new JSONObject();
+        object.put("UP","true");
+        return object.toString();
+    }
+}
